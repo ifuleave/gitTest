c7ae3e0da594615e8b1fb745d371250ff81d72f6
sujin <ifuleav_e@naver.com>
2023-04-17 18:00:49 +0900

    566 페이지까지

--- src/main/java/org/zerock/b01/controller/ReplyController.java
+++ src/main/java/org/zerock/b01/controller/ReplyController.java
@@ -4,13 +4,11 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.zerock.b01.dto.PageRequestDTO;
+import org.zerock.b01.dto.PageResponseDTO;
 import org.zerock.b01.dto.ReplyDTO;
 import org.zerock.b01.service.ReplyService;
 
@@ -25,21 +23,7 @@ import java.util.Map;
 @RequiredArgsConstructor // 의존성주입을 위한 
 public class ReplyController {
     private final ReplyService replyService; //replyService 주입
-
-//    @ApiOperation(value = "Replies POST", notes = "POST 방식으로 댓글 등록")
-//    @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
-//    public Map<String,Long> register(@Valid @RequestBody ReplyDTO replyDTO, BindingResult bindingResult) throws BindException {
-//        log.info(replyDTO);
-//        if(bindingResult.hasErrors()){
-//            throw new BindException(bindingResult);
-//        }
-//
-//        Map<String,Long> resultMap = new HashMap<>();
-//        resultMap.put("rno",111L);
-//
-//        return resultMap;
-//    }
-
+    
 
     //등록 기능 확인 
     @ApiOperation(value = "Replies POST", notes = "POST 방식으로 댓글 등록")
@@ -55,6 +39,46 @@ public class ReplyController {
         Long rno = replyService.register(replyDTO);
         resultMap.put("rno",rno);
         return resultMap;
+    }
+    
+    @ApiOperation(value = "Replies of Board", notes = "GET 방식으로 특정 게시물의 댓글 목록")
+    @GetMapping(value = "/list/{bno}")
+    public PageResponseDTO<ReplyDTO> getList(@PathVariable("bno") Long bno, PageRequestDTO pageRequestDTO){
+                                            // 호출하는 경로의 값을 파라미터의 변수로 처리할 수 있는 방법을 제공
+        PageResponseDTO<ReplyDTO> responseDTO = replyService.getListOfBoard(bno,pageRequestDTO);
+
+        return responseDTO;
+    }
+    
+    //특정한 댓글을 조회할 떄 Reply의 rno를 경로로 이용해서 Get 방식으로 처리한다
+    //웹브라우저에 댓글 번호 입력
+    @ApiOperation(value = "Read Reply",notes = "GET 방식으로 특정 댓글 조회")
+    @GetMapping("/{rno}")
+    public ReplyDTO getReplyDTO(@PathVariable("rno") Long rno){
+         ReplyDTO replyDTO = replyService.read(rno);
+         return replyDTO;
+    }
+    
+    //삭제
+    @ApiOperation(value = "Delete Reply", notes = "DELETE 방식으로 특정 댓글 삭제")
+    @DeleteMapping("/{rno}")
+    public Map<String,Long> remove(@PathVariable("rno") Long rno){
+        replyService.remove(rno);
+        Map<String, Long> resultMap = new HashMap<>();
+        resultMap.put("rno", rno);
+        return resultMap;
+    }
+
+    //특정 댓글 수정
+    @ApiOperation(value = "Modify Reply", notes = "PUT 방식으로 특정 댓글 수정")
+    @PutMapping(value = "/{rno}",consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Long> remove(@PathVariable("rno") Long rno, @RequestBody ReplyDTO replyDTO){
+                                                                //  jason 문자열이 전송되므로 이를 처리
+            replyDTO.setRno(rno); //번호를 일치시킴
+            replyService.modify(replyDTO);
+            Map<String, Long> resultMap = new HashMap<>();
+            resultMap.put("rno",rno);
+            return resultMap;
     }
 
 }
--- src/main/java/org/zerock/b01/controller/advice/CustomRestAdvice.java
+++ src/main/java/org/zerock/b01/controller/advice/CustomRestAdvice.java
@@ -2,6 +2,7 @@ package org.zerock.b01.controller.advice;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 /*외부에서 아작스로 댓글 등록 기능 호출 시 500에러(서버에러)발생한다면
 호출한 측에서 서버 문제라 생각할 것이고 데이터에 문제라고 생각하지 않을 것
@@ -50,7 +52,20 @@ public class CustomRestAdvice {
         Map<String, String> errorMap = new HashMap<>();
 
         errorMap.put("time",""+System.currentTimeMillis());
-        errorMap.put("msg", "constraint 실패"); //메세지 띄워주기 
+        errorMap.put("msg", "constraint 실패"); //메세지 띄워주기
+        return ResponseEntity.badRequest().body(errorMap);
+    }
+
+    //댓글 번호 예외처리 ( 없는 번호 댓글 삭제시 예외처리)
+    @ExceptionHandler({NoSuchElementException.class, EmptyResultDataAccessException.class})
+    @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+    public ResponseEntity<Map<String, String>> handleNoSuchElement(Exception e){
+        log.error(e);
+
+        Map<String, String> errorMap = new HashMap<>();
+
+        errorMap.put("time","" + System.currentTimeMillis());
+        errorMap.put("msg","No Such Element Exception 댓글 번호 에러");
         return ResponseEntity.badRequest().body(errorMap);
     }
 
