d56e40d2161d662516869751d1fa1b8e149fed64
sujin <ifuleav_e@naver.com>
2023-04-22 22:13:02 +0900

    580까지 댓글, 페이지 목록 출력

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -1,5 +1,5 @@
 
-async function get1(bno){
-    const result = await axios.get(`/replies/list/${bno}`)
-    return result
+async function getList({bno, page, size,goLast}){
+    const result = await axios.get(`/replies/list/${bno}`,{param:{page,size}})
+    return result.data
 }
