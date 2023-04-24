8b070dfb2dc5a174a69d05a90b561e15b80ee818
sujin <ifuleav_e@naver.com>
2023-04-25 00:04:51 +0900

    586까지 댓글츄가

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -1,5 +1,16 @@
 
 async function getList({bno, page, size,goLast}){
     const result = await axios.get(`/replies/list/${bno}`,{param:{page,size}})
+
+    if (goLast){
+        const total = result.data.total
+        const lastPage = parseInt(Math.ceil(total/size))
+        return getList({bno:bno, page:lastPage, size:size})
+    }
     return result.data
+}
+
+async function addReply(replyObj){
+    const response = await axios.post(`/replies/`,replyObj)
+    return response.data
 }
